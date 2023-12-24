99549ded095b3e47738ed8fa4eebab9affb449e0
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-12-25 02:59:14 +0600

    Create a filter context and useFilter hook

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,14 +1,19 @@
+'use client'
+
 import Link from 'next/link'
 import { Category, Media } from '../../../../payload/payload-types'
 import classes from './index.module.scss'
+import { useFilter } from '../../../_providers/Filters'
 
 const CategoryCard = ({ category }: { category: Category }) => {
   const media = category.media as Media
+  const { setCategoryFilters } = useFilter()
 
   return (
     <Link
       href="/products"
       className={classes.card}
+      onClick={() => setCategoryFilters([category.id])}
       style={{ backgroundImage: `url(${media.url})` }}
     >
       <p className={classes.title}>{category.title}</p>
